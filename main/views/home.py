cae123ea06a203bfb3378f17a3cdadf9b713485b
rlichiere <rlichiere@visiativ.com>
2019-06-01 08:23:09 +0200

    # cleanup

--- main/views/home.py
+++ main/views/home.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import TemplateView
 
+from ..generic.views import ViewHelper
 
-class HomeView(TemplateView):
+
+class HomeView(ViewHelper, TemplateView):
     template_name = 'main/home.html'
 
     def get_context_data(self, **kwargs):
+        _executor = self.request.user
+        self.log.startView(_executor)
+
         context = super(HomeView, self).get_context_data(**kwargs)
 
+        self.log.endView()
         return context
